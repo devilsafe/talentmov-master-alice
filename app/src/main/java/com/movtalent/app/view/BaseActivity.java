538f7dc61de7eec6ebe5b821e35e52df8689a14f
devilsafe <devilsafe@163.com>
2020-06-05 14:46:01 +0800

    ad_banner.html ModeUtil.java App_Config.java BaseActivity.java

--- app/src/main/java/com/movtalent/app/view/BaseActivity.java
+++ app/src/main/java/com/movtalent/app/view/BaseActivity.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import com.leaf.library.StatusBarUtil;
+import com.movtalent.app.R;
 
 /**
  * @author huangyong
@@ -19,7 +21,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
-       // StatusBarUtil.setColor(this, getResources().getColor(R.color.select_color));
+        StatusBarUtil.setColor(this, getResources().getColor(R.color.select_color));
         bind = ButterKnife.bind(this);
         initView();
         initData();
