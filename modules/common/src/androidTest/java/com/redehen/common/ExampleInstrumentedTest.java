afdabd63ea912e6d1aa94128234188a0113fbe41
devilsafe <devilsafe@163.com>
2020-06-10 11:47:35 +0800

    androidx

--- modules/common/src/androidTest/java/com/redehen/common/ExampleInstrumentedTest.java
+++ modules/common/src/androidTest/java/com/redehen/common/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
-package com.lib.common;
+package com.redehen.common;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,13 +13,13 @@ import static org.junit.Assert.assertEquals;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.lib.common.test", appContext.getPackageName());
-    }
-}
+//@RunWith(AndroidJUnit4.class)
+//public class ExampleInstrumentedTest {
+//    @Test
+//    public void useAppContext() {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        assertEquals("com.lib.common.test", appContext.getPackageName());
+//    }
+//}
